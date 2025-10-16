55a8112594d040c343d33b0986bd44160ce4895c
Asher Peer <asher.colman21@gmail.com>
2025-10-16 16:17:06 +0300

    new about and apartmentment features

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -5,10 +5,10 @@ export default function About() {
                 About Us
             </h2>
             <p style={{ textAlign: "center", fontSize: "1.2rem", lineHeight: "1.6" }}>
-                We offer modern, fully furnished apartments for short and long stays.
-                With our partnership with Booking and Airbnb, you can enjoy a simple,
-                secure, and comfortable experience. Whether you’re here for business or
-                vacation – we’ve got the perfect place for you.
+                ברוכים הבאים לבית השני שלכם – המקום שבו נוחות, אלגנטיות וחום אנושי נפגשים.
+                אצלנו כל דירה עוצבה בקפידה כדי להעניק לכם תחושת רוגע, יוקרה ונוחות מושלמת – מהרגע שתכנסו ועד לרגע שתעזבו.
+                בשילוב עם Booking ו-Airbnb, אנו מבטיחים חוויית אירוח בטוחה, נעימה וחסרת דאגות.
+                בין אם אתם כאן לחופשה קסומה או לשהייה עסקית, אנו מזמינים אתכם ליהנות מאירוח ברמה אחרת – כזה שנשאר בלב הרבה אחרי העזיבה
             </p>
         </section>
     );
